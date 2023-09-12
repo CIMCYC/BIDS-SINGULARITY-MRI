bbb03c4408d5edd4f94884b2ee5700b845658beb
pilarsanpe <125973574+PilarSanPe@users.noreply.github.com>
2023-09-12 12:46:18 +0200

    updated

--- HEURISTIC_FILE.py
+++ HEURISTIC_FILE.py
@@ -22,11 +22,22 @@ s - follow python string module:
     # Modify this bit to include all the scans that you want to convert into BIDS.
     # This should be the format: VARIABLE_NAME = create_key('PATH/filename')
     variable_name = create_key('sub-{subject}/{session}/{datatype}/sub-{subject}_{session}_{filename}}') #change variable_name to the name according to the type of image. Filename would be the name following the BIDS structure according to the type of image.
+    #examples:
+    #t1w = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_T1w')
+    #run1 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task_run-1_bold')
+    #distPA = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_dir-PA_epi')
+    #distAP = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_dir-AP_epi')
+    #B1k = create_key('sub-{subject}/dwi/sub-{subject}_{session}_B1k')
     ...
-
     
     # Now also add the new keys also here
     info = {variable_name: [],
+            #examples:
+            #t1w: [],
+            #run1: [],
+            #distPA: [],
+            #distAP: [],
+            #B1k: [],
             ...
            }
 
@@ -34,6 +45,17 @@ s - follow python string module:
 	# Now add here a new "if" statement for the keys that were created above.
         if s.protocol_name == 'name': #enter the name of the assigned scanner image for each particular variable_name.
             info[variable_name] = [s.series_id]
+        #examples:
+        #if s.protocol_name == 'MPRAGE_1mm_new':
+            #info[t1w] = [s.series_id]
+        #if s.protocol_name == 'FeedBES_2mm_run1':
+            #info[run1] = [s.series_id]
+        #if s.protocol_name == 'FeedBES_correction_posBlip':
+            #info[distAP] = [s.series_id]
+        #if s.protocol_name == 'FeedBES_correction_negBlip':
+            #info[distPA] = [s.series_id]
+        #if s.protocol_name == 'MBep2d_diff_175iso_B1k_d32_blip':
+            #info[B1k] = [s.series_id]
         ...       
 
     return info
--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-CONVERT MRI DATA TO BIDS FORMAT 
+CONVERT MRI (FMRI, DWI...) DATA TO BIDS FORMAT
  
 Steps to use this repository: 
 1. Download the Docker container: https://www. docker. com. 
@@ -16,11 +16,13 @@ docker run --rm -it \
 -v 'C:\Users\Usuario\Documents\MRI/:/dataheu' \ #write the path where you have your configuration file on your computer followed by ‘:/' and the name that you want to assign it to the folder where you will put it inside docker, any one. 
 -v 'C:\Users\User\Documents\MRI\BIDS/:/output' \ #write the path where you want to export your BIDS on your computer followed by ‘:/' and the name that you want to assign it to the folder where you are going to do it inside docker, any one. 
 nipy/heudiconv:latest \ #call the docker image used as code to go to BIDS format. 
--s '01' \ #participant number. 
+-s '01' \ #participant number (not letters). 
 -d dicoms/{subject} \  
 --ses '01' \ #session number 
 -f dataheu/HEURISTIC_ARCHIVE. py \ #path of the configuration file on our computer. 
 -c dcm2niix -b \ #NIfTI converter. 
 -o /output --minmeta 
  
+All the images of the same participant must be together, they cannot be classified in folders according to each type of protocol.
+It is advisable to name the participant folder as 001, 002..., because it is easier for the code to rename the subject folders if they are only numerical values. The heuristic file already does the renaming according to the BIDS structure. 
 If you work on Windows, almost certainly, you will have to write everything on one line by removing the '\' slashes for it to run. 
